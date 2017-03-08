cede169d0664253519f9a2512a8c2343ae0a51c4
Saranya Ganapathy <SaranyaGanapathy@Saranyas-MBP.home>
2017-03-07 19:45:23 -0500

    fixed js and album page

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,17 @@
 var pointsArray = document.getElementsByClassName('point');
+
+
 var animatePoints = function() {
 
-   
     var revealPoint = function(index) {
-                points[0].style.opacity = 1
-                points[0].style.transform = "scaleX(1) translateY(0)";
-                points[0].style.msTransform = "scaleX(1) translateY(0)";
-                points[0].style.WebkitTransform = "scaleX(0) translateY(0)";
+        pointsArray[index].style.opacity = 1;
+        pointsArray[index].style.transform = "scaleX(1) translateY(0)";
+        pointsArray[index].style.msTransform = "scaleX(1) translateY(0)";
+        pointsArray[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
-    
-    for (var i = 0; i < points.length; i++) {
-        revealPoint(i);
 
+    for (var i = 0; i < pointsArray.length; i++) {
+        revealPoint(i);
     }
 };
 
@@ -22,8 +22,8 @@ window.onload = function() {
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200; 
     window.addEventListener('scroll', function(event) {
-         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);
-         }
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
     });
 }
