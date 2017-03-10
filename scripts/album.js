06c47e4ebb3307cbb04634e317847ab37ba35482
Saranya Ganapathy <SaranyaGanapathy@Saranyas-MBP.home>
2017-03-10 15:48:00 -0500

    assignment 11 completed

--- scripts/album.js
+++ scripts/album.js
@@ -26,46 +26,71 @@ var albumMarconi = {
     songs: [
         { title: 'Hello, Operator?', duration: '1:01' }, 
         { title: 'Ring, ring, ring', duration: '5:01' }, 
-        { title: 'Fits in your pocket', duration: '3:21' }
-        { title: 'Can you hear me now?', duration: '3:14'}
+        { title: 'Fits in your pocket', duration: '3:21' },
+        { title: 'Can you hear me now?', duration: '3:14'}, 
         { title: 'Wrong phone number', duration: '2:15'}
     ]
+};
+
+var albumCulture = {
+    title: 'Culture', 
+    artist: 'Migos',
+    label: 'The Label', 
+    year: '2017',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello', duration: '2:00' },
+        { title: 'Bad and Boujee', duration: '3:40'},
+        { title: 'TShirt', duration: '4:00'}
+
+    ]
+
 };
 
 var createSongRow = function(songNumber, songName, songLength) {
     var template = 
         '<tr class ="album-view-song-item">'
-        '   <td class="song-item-number">' + songNumbers + '</td>'
-        '   <td class="song-item-title">' + songName + '</td>'
-        '   <td class="song-item-duration>' + songLength + '</td>'
-        '</tr>'
-        ;
-    
+    '   <td class="song-item-number">' + songNumbers + '</td>'
+    '   <td class="song-item-title">' + songName + '</td>'
+    '   <td class="song-item-duration>' + songLength + '</td>'
+    '</tr>'
+    ;
+
     return template;
 };
-
+//#1
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+// #2
 var setCurrentAlbum = function(album) {
-    //#1
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-    // #2
+
 
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
     albumImage.setAttribute('src', album.albumArtUrl);
-    
+
     // #3
     albumSongList.innerHTML = ' ';
     // #4
     for (var i = 0; i < album.songs.length; i++) {
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
-        }
+    }
 };
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
-}
+
+    var album = [albumPicasso, albumMarconi, albumCulture];
+    var index = 0;
+    albumImage.addEventListener('click', function(event) {
+        setCurrentAlbum(album[index]);
+        index++;
+        if (index == albums.length) {
+            index = 0;
+        }
+    });
+};
