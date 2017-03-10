442993c7a19e24f6bd895d1f61e34de6c7b3a60b
Saranya Ganapathy <SaranyaGanapathy@Saranyas-MBP.home>
2017-03-10 18:26:42 -0500

    fixed album javascript

--- scripts/album.js
+++ scripts/album.js
@@ -1,5 +1,4 @@
 // example album
-
 var albumPicasso = {
     title: 'The Colors',
     artist: 'Pablo Picasso',
@@ -35,12 +34,12 @@ var albumMarconi = {
 var createSongRow = function(songNumber, songName, songLength) {
     var template = 
         '<tr class ="album-view-song-item">'
-        '   <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber +  '</td>'
-        '   <td class="song-item-title">' + songName + '</td>'
-        '   <td class="song-item-duration>' + songLength + '</td>'
-        '</tr>'
-        ;
-    
+    + '   <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber +  '</td>'
+    + '   <td class="song-item-title">' + songName + '</td>'
+    + '   <td class="song-item-duration>' + songLength + '</td>'
+    + '</tr>'
+    ;
+
     return template;
 };
 
@@ -52,18 +51,16 @@ var setCurrentAlbum = function(album) {
     var albumImage = document.getElementsByClassName('album-cover-art')[0];
     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
     // #2
-
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
     albumImage.setAttribute('src', album.albumArtUrl);
-    
     // #3
-    albumSongList.innerHTML = ' ';
+    albumSongList.innerHTML = '';
     // #4
     for (var i = 0; i < album.songs.length; i++) {
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
-        }
+    }
 };
 
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
@@ -71,20 +68,21 @@ var songRows = document.getElementsByClassName('album-view-song-item');
 
 //Album button templates
 var playButtonTemplate = '<a class = "album-song-button"><span class="ion-play"></span></a>';
+
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
-    
+
     songListContainer.addEventListener('mouseover', function(event){
-       // Only target individual song rows during event delegation
+        // Only target individual song rows during event delegation
         if (event.target.parentElement.className === 'album-view-song-item'){
-         event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;   
-    
+            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;   
+
         }
     });
-    
+
     for (var i = 0; i < songRows.length; i++) {
         songRows[i].addEventListener('mouseleave', function(event) {
-         //selects first child element, which is the song-item-number element
+            //selects first child element, which is the song-item-number element
             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
         });
     }
